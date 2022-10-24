55d523950f3a03fb23d64dae9c5d795996cc4f2f
hoon2 <sanghoon.engineer@gmail.com>
2022-10-25 07:43:12 +0900

    css-flex코드

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import GlobalStyle from "./GlobalStyle";
+import styled from "styled-components";
+
+const ParentBox = styled.div`
+  display: flex;
+  height: 300px;
+  justify-content: center;
+  align-items: flex-start;
+  background-color: aliceblue;
+`;
+
+const ChildBox = styled.div`
+  font-size: 18px;
+  text-align: center;
+  width: 150px;
+  height: 150px;
+  margin: 10px;
+  padding: 5px;
+  box-sizing: border-box;
+  color: white;
+  background-color: dodgerblue;
+`;
+
+const GrowBox = styled(ChildBox)`
+  flex-grow: 1;
+`;
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <div>안녕</div>
+      <ParentBox>
+        <ChildBox>child 1</ChildBox>
+        <GrowBox>child 2</GrowBox>
+        <ChildBox>child 3</ChildBox>
+      </ParentBox>
     </>
   );
 }
